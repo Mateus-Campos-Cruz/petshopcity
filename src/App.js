6e464f83a482f0d9f9765dbd8f23bf0989bab7b9
mateus.cruz <mateusfelipecamposdacruz@gmail.com>
2024-04-25 00:15:54 -0300

    Subindo atualizações

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import './style.css';
+import ptBR from 'date-fns/locale/pt-BR'; // Importe o locale para português do Brasil
 
 function App() {
+  const [data, setData] = useState(null); // Alterado para null para iniciar sem data selecionada
+  const [quantidadePequenos, setQuantidadePequenos] = useState('');
+  const [quantidadeGrandes, setQuantidadeGrandes] = useState('');
+  const [resultado, setResultado] = useState({ melhorPetshop: '', precoTotal: 0 });
+  const [erro, setErro] = useState('');
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setErro('');
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [erro]);
+
+  function handleSearch(e) {
+    e.preventDefault();
+  
+    try {
+      if (!data || !quantidadePequenos || !quantidadeGrandes || quantidadePequenos === '0' || quantidadeGrandes === '0') {
+        throw new Error('Preencha todas as informações corretamente antes de prosseguir.');
+      }
+  
+      const diaSemana = data.getDay();
+  
+      if (diaSemana < 0 || diaSemana > 6 || isNaN(diaSemana)) {
+        throw new Error('Data inválida. Preencha todas informações antes de prosseguir.');
+      }
+  
+      const petshops = [
+        { nome: 'Meu Canino Feliz', distancia: 2, precoPequenosSemana: 20, precoGrandesSemana: 40, precoPequenosFds: 24, precoGrandesFds: 48 },
+        { nome: 'Vai Rex', distancia: 1.7, precoPequenosSemana: 15, precoGrandesSemana: 50, precoPequenosFds: 20, precoGrandesFds: 55 },
+        { nome: 'ChowChawgas', distancia: 0.8, precoPequenos: 30, precoGrandes: 45 }
+      ];
+  
+      const calcularPrecoTotal = (petshop) => {
+        const precoPequenos = (diaSemana >= 1 && diaSemana <= 5) ? petshop.precoPequenosSemana : petshop.precoPequenosFds;
+        const precoGrandes = (diaSemana >= 1 && diaSemana <= 5) ? petshop.precoGrandesSemana : petshop.precoGrandesFds;
+        const precoTotal = (parseInt(quantidadePequenos) * precoPequenos) + (parseInt(quantidadeGrandes) * precoGrandes);
+        return { nome: petshop.nome, precoTotal };
+      };
+  
+      const precosTotais = petshops.map(calcularPrecoTotal);
+      const melhorPetshop = precosTotais.reduce((melhor, atual) => (atual.precoTotal < melhor.precoTotal ? atual : melhor));
+  
+      setResultado({ melhorPetshop: melhorPetshop.nome, precoTotal: melhorPetshop.precoTotal });
+      setErro('');
+
+      setData(null);
+      setQuantidadePequenos('');
+      setQuantidadeGrandes('');
+
+    } catch (error) {
+      setErro(error.message);
+    }
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="logo">
+        <img src={process.env.PUBLIC_URL + '/img/logo.png'} alt=""></img>
+      </div>
+
+      <h1 className="title">Pet Shop City</h1>
+
+      <div className="containerForm">
+        <form>
+          <DatePicker
+            selected={data}
+            onChange={(date) => setData(date)}
+            dateFormat="dd/MM/yyyy"
+            locale={ptBR} // Definindo o locale para português do Brasil
+            placeholderText="Escolha uma data" // Texto do placeholder
+          />
+          <input type="text" placeholder="Digite a quantidade de cães pequenos..." value={quantidadePequenos} onChange={(e) => setQuantidadePequenos(e.target.value)} />
+          <input type="text" placeholder="Digite a quantidade de cães grandes..." value={quantidadeGrandes} onChange={(e) => setQuantidadeGrandes(e.target.value)} />
+          <button type="submit" className="buttonSearch" onClick={handleSearch}>Calcular</button>
+        </form>
+        {erro && <div className="erro">{erro}</div>}
+      </div>
+
+      {resultado.melhorPetshop && (
+        <main className="main">
+          <h2>Resultado do cálculo</h2>
+          <span>Melhor canil: {resultado.melhorPetshop}</span>
+          <span>Preço total dos banhos: R$ {resultado.precoTotal.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
+        </main>
+      )}
+
     </div>
   );
 }
